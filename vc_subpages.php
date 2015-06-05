97a5eb313e5e3d31101b6e912019f68b9c4f0a3a
gmlion <gm_l@email.it>
2015-06-05 15:49:13 +0200

    Sistemazione commenti

--- vc_subpages.php
+++ vc_subpages.php
@@ -7,7 +7,7 @@ Version: 0.1
 Author: Gianmarco Leone
 Author URI: http://wpbakery.com
 License: GPLv2 or later
-Icons made by Stephen Hutchings from www.flaticon.com and licensed by CC BY 3.0
+Icons made by Stephen Hutchings from www.flaticon.com is licensed by CC BY 3.0
 */
 
 /*
@@ -19,9 +19,10 @@ if (!defined('ABSPATH')) die('-1');
 
 class VCSubpagesAddonClass {
     function __construct() {
-        // VC integration
+        // We safely integrate with VC with this hook
         add_action( 'init', array( $this, 'integrateWithVC' ) );
  
+        // Use this when creating a shortcode addon
         add_shortcode( 'bartag', array( $this, 'renderMyBartag' ) );
 
         // Register CSS and JS
@@ -36,23 +37,79 @@ class VCSubpagesAddonClass {
             return;
         }
  
-        
+        /*
+        Add your Visual Composer logic here.
+        Lets call vc_map function to "register" our custom shortcode within Visual Composer interface.
+
+        More info: http://kb.wpbakery.com/index.php?title=Vc_map
+        */
+        /*vc_map( array(
+            "name" => __("My Bar Shortcode", 'vc_subpages'),
+            "description" => __("Bar tag description text", 'vc_subpages'),
+            "base" => "bartag",
+            "class" => "",
+            "controls" => "full",
+            "icon" => plugins_url('assets/asterisk_yellow.png', __FILE__), // or css class name which you can reffer in your css file later. Example: "vc_subpages_my_class"
+            "category" => __('Content', 'js_composer'),
+            //'admin_enqueue_js' => array(plugins_url('assets/vc_subpages.js', __FILE__)), // This will load js file in the VC backend editor
+            //'admin_enqueue_css' => array(plugins_url('assets/vc_subpages_admin.css', __FILE__)), // This will load css file in the VC backend editor
+            "params" => array(
+                array(
+                  "type" => "textfield",
+                  "holder" => "div",
+                  "class" => "",
+                  "heading" => __("Text", 'vc_subpages'),
+                  "param_name" => "foo",
+                  "value" => __("Default params value", 'vc_subpages'),
+                  "description" => __("Description for foo param.", 'vc_subpages')
+              ),
+              array(
+                  "type" => "colorpicker",
+                  "holder" => "div",
+                  "class" => "",
+                  "heading" => __("Text color", 'vc_subpages'),
+                  "param_name" => "color",
+                  "value" => '#FF0000', //Default Red color
+                  "description" => __("Choose text color", 'vc_subpages')
+              ),
+              array(
+                  "type" => "textarea_html",
+                  "holder" => "div",
+                  "class" => "",
+                  "heading" => __("Content", 'vc_subpages'),
+                  "param_name" => "content",
+                  "value" => __("<p>I am test text block. Click edit button to change this text.</p>", 'vc_subpages'),
+                  "description" => __("Enter your content.", 'vc_subpages')
+              ),
+            )
+        ) );*/
         vc_map( array(
             "name" => __("Subpages List", 'vc_subpages'),
             "description" => __("Show a list of all subpages", 'vc_subpages'),
             "base" => "bartag",
             "class" => "vc_subpages",
             "controls" => "full",
-            "icon" => plugins_url('assets/th.png', __FILE__),
+            "icon" => plugins_url('assets/th.png', __FILE__), // or css class name which you can reffer in your css file later. Example: "vc_subpages_my_class"
             "category" => __('Content', 'js_composer'),
+            //'admin_enqueue_js' => array(plugins_url('assets/vc_subpages.js', __FILE__)), // This will load js file in the VC backend editor
+            //'admin_enqueue_css' => array(plugins_url('assets/vc_subpages_admin.css', __FILE__)), // This will load css file in the VC backend editor
             ) );
     }
     
     /*
-    Shortcode logic
+    Shortcode logic how it should be rendered
     */
     public function renderMyBartag( $atts, $content = null ) {
-      /*Query for child pages*/
+      /*extract( shortcode_atts( array(
+        'foo' => 'something',
+        'color' => '#FF0000'
+      ), $atts ) );
+      $content = wpb_js_remove_wpautop($content, true); // fix unclosed/unwanted paragraph tags in $content
+     
+      $output = "<div style='color:{$color};' data-foo='${foo}'>{$content}</div>";
+      return $output;*/
+      
+      /*query for child pages*/
       global $post;
         
       query_posts(array(
@@ -87,15 +144,18 @@ class VCSubpagesAddonClass {
     }
 
     /*
-    Load plugin css and javascript files
+    Load plugin css and javascript files which you may need on front end of your site
     */
     public function loadCssAndJs() {
       wp_register_style( 'vc_subpages_style', plugins_url('assets/vc_subpages.css', __FILE__) );
       wp_enqueue_style( 'vc_subpages_style' );
+
+      // If you need any javascript files on front end, here is how you can load them.
+      //wp_enqueue_script( 'vc_subpages_js', plugins_url('assets/vc_subpages.js', __FILE__), array('jquery') );
     }
 
     /*
-    Show notice if plugin is activated but Visual Composer is not
+    Show notice if your plugin is activated but Visual Composer is not
     */
     public function showVcVersionNotice() {
         $plugin_data = get_plugin_data(__FILE__);
